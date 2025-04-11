293c3d3590af075f127bcfdcde3cecd6fc720a4d
Johan Karlsson <johkar2@kth.se>
2025-04-11 14:19:53 +0200

    First prototype with two prelimary pages and Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route, NavLink, BrowserRouter as Router, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <h1> festis</h1>
+          <ul className = "header">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/About">about</NavLink></li>
+          </ul>
+          <div className ="pageContent">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+            </Routes>
+          </div>
+        </div>
+      </Router>
   );
 }
 
