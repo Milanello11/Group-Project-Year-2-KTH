cddfea03c2d2efbfd12a30e4663f4a9d3e6fd71c
Ismail Mohammed <isse_a@hotmail.com>
2025-04-12 18:12:03 +0200

    Fixed hosting for backend and database

--- backend/src/main/java/com/kth/snomos/backend/Controller/Controller.java
+++ backend/src/main/java/com/kth/snomos/backend/Controller/Controller.java
@@ -3,10 +3,12 @@ package com.kth.snomos.backend.Controller;
 import com.kth.snomos.backend.Entity.User;
 import com.kth.snomos.backend.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class Controller {
 
--- backend/src/main/java/com/kth/snomos/backend/Repository/UserRepo.java
+++ backend/src/main/java/com/kth/snomos/backend/Repository/UserRepo.java
@@ -3,5 +3,5 @@ package com.kth.snomos.backend.Repository;
 import com.kth.snomos.backend.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User, Integer> {
+public interface UserRepo extends JpaRepository<User, Long> {
 }
