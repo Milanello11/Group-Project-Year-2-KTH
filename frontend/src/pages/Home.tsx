962bffb1dd66ec9d1f525ad25fb6d9c8fb120291
Max Masuch <masuch@kth.se>
2025-04-16 13:54:50 +0200

    made function call from home to festivalComponent

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './Home.css';
+import FestivalBox from "../components/FestivalBox";
+
+type Festival = {
+    festivalId: number;
+    festivalName: string;
+    festivalLocation: string;
+    festivalDate: string;
+    ticketsLeft: number;
+};
 
 const Home = () => {
+    const [festivals, setFestivals] = useState<Festival[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/festival/findall')
+            .then((response) => response.json())
+            .then((data) => setFestivals(data.slice(0, 3)))
+            .catch((error) => console.error('Error fetching festivals:', error));
+    }, []);
+
     return (
         <div className ="background">
             <div className ="backgroundPicture">
-                <h1 style={{ color:'white'}}>Welcome to the HomePage</h1>
+                {festivals.map((festival) => (
+                    <FestivalBox
+                        festivalId={festival.festivalId}
+                        festivalName={festival.festivalName}
+                        festivalLocation={festival.festivalLocation}
+                        festivalDate= {festival.festivalDate}
+                        ticketsLeft = {festival.ticketsLeft}
+                    />
+                ))}
             </div>
         </div>
     );
