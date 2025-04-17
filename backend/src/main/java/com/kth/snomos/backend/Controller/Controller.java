bdfe3113d2a77cbce0118b185c126bb7bd81e8d3
Ismail Mohammed <isse_a@hotmail.com>
2025-04-17 14:10:07 +0200

    changed description update so that it uses PUT instetad of POST

--- backend/src/main/java/com/kth/snomos/backend/Controller/Controller.java
+++ backend/src/main/java/com/kth/snomos/backend/Controller/Controller.java
@@ -7,7 +7,6 @@ import com.kth.snomos.backend.Entity.User;
 import com.kth.snomos.backend.Service.FestivalService;
 import com.kth.snomos.backend.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -38,7 +37,7 @@ public class Controller {
         return userService.findAll();
     }
 
-    @GetMapping("/user/findbyname/{username}/{password}")
+    @GetMapping("/user/find/{username}/{password}")
     public long findByName(@PathVariable String username, @PathVariable String password) {
         return userService.userExists(username, password);
     }
@@ -83,6 +82,11 @@ public class Controller {
     @GetMapping("/festival/upcoming")
     public List<Festival> getUpcomingFestivals() {
         return festivalService.getUpcomingFestivals();
+    }
+
+    @PutMapping("/festival/update/description/{festivalId}")
+    public void updateFestivalDescription(@PathVariable long festivalId, @RequestBody String description) {
+        festivalService.updateFestivalDescription(festivalId, description);
     }
 
     @PostMapping("/festival/save")
