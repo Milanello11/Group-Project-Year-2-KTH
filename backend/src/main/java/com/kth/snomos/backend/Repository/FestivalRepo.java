3c8c3b43b46f2b0886f01a562aeaa4b1e9d1c9a0
eliasBossen <eliapa@kth.se>
2025-04-24 09:09:38 +0200

    Changed queries from equals to like to enable partial search

--- backend/src/main/java/com/kth/snomos/backend/Repository/FestivalRepo.java
+++ backend/src/main/java/com/kth/snomos/backend/Repository/FestivalRepo.java
@@ -18,19 +18,19 @@ public interface FestivalRepo extends JpaRepository<Festival, Long> {
     @Query(value = "SELECT * FROM festival where festival_date = :date AND festival_name = :name", nativeQuery = true)
     Festival findFestivalByDateAndName(@Param("date") LocalDate date,@Param("name") String name );
 
-    @Query(value = "SELECT * FROM festival WHERE festival_name = :name", nativeQuery = true)
+    @Query(value = "SELECT * FROM festival WHERE festival_name LIKE :name", nativeQuery = true)
     List<Festival> findFestivalByName(@Param("name") String name);
 
     @Query(value = "SELECT * FROM festival WHERE festival_date = :date", nativeQuery = true)
     List<Festival> findFestivalByDate(@Param("date") LocalDate festival_date);
 
-    @Query(value = "SELECT * FROM festival WHERE festival_location = :location", nativeQuery = true)
+    @Query(value = "SELECT * FROM festival WHERE festival_location LIKE :location ", nativeQuery = true)
     List<Festival> findFestivalByLocation(@Param("location") String location);
 
     @Query(value = "SELECT f.* FROM festival f " +
             "JOIN artist_festival af ON f.festival_id = af.festival_id " +
             "JOIN artist a ON a.artist_name = af.artist_name " +
-            "WHERE a.artist_name = :artist", nativeQuery = true)
+            "WHERE a.artist_name LIKE :artist", nativeQuery = true)
     List<Festival> findFestivalByArtist(@Param("artist") String artist);
 
     @Query(value = "SELECT a.* FROM artist a JOIN artist_festival af on a.artist_name = af.artist_name " +
