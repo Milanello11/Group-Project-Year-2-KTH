93c8e5c1e7af541d203e6192f3677ce5b2775c2d
Ismail Mohammed <isse_a@hotmail.com>
2025-05-07 16:01:55 +0200

    removed unused import

--- frontend/src/components/LogInMenu.tsx
+++ frontend/src/components/LogInMenu.tsx
@@ -1,12 +1,10 @@
-import {Box, Input, Stack, Button, Collapsible, Flex} from "@chakra-ui/react";
+import {Box, Input, Stack, Button, Collapsible, Flex, Spinner} from "@chakra-ui/react";
 import styles from "./LogInMenu.module.css";
 import React, { useState } from "react";
 import { useAuth } from "./context/AuthContext";
 import {NavLink, useNavigate} from "react-router-dom";
 import {CircleUserRound} from "lucide-react";
 import { toaster } from "./ui/toaster"
-import {Spinner} from "@chakra-ui/react";
-
 
 const LogInMenu = () => {
     const { user, login, logout } = useAuth();
