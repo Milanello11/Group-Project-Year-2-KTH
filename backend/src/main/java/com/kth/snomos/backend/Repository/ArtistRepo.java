491703b19b670fe3b4e6c982a4cdaa56c0b39b9f
Ismail Mohammed <isse_a@hotmail.com>
2025-05-09 08:08:11 +0200

    changed artist query so it returns Artist object instead of Artist name only as a string

--- backend/src/main/java/com/kth/snomos/backend/Repository/ArtistRepo.java
+++ backend/src/main/java/com/kth/snomos/backend/Repository/ArtistRepo.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ArtistRepo extends JpaRepository<Artist, String> {
-    @Query(value = "SELECT a.artist_name FROM artist a JOIN artist_festival af ON a.artist_name = af.artist_name " +
+    @Query(value = "SELECT * FROM artist a JOIN artist_festival af ON a.artist_name = af.artist_name " +
             "WHERE af.festival_id = :festivalId", nativeQuery = true)
-    List<String> getAllArtistsFromFestival(@Param("festivalId")long festivalId);
+    List<Artist> getAllArtistsFromFestival(@Param("festivalId")long festivalId);
 }
