f3365f1813f28e125b14b6a4a1ba2390702018ec
eliasBossen <eliapa@kth.se>
2025-05-12 14:18:17 +0200

    preperation for update festival in backend

--- backend/src/main/java/com/kth/snomos/backend/Service/FestivalService.java
+++ backend/src/main/java/com/kth/snomos/backend/Service/FestivalService.java
@@ -55,7 +55,6 @@ public class FestivalService {
         return festivalRepo.findById(id).orElseThrow();
     }
 
-
     public Artist findArtistByName(String name) {
         return artistRepo.existsByNameLike(name) ? artistRepo.findArtistByName(name) : null;
     }
